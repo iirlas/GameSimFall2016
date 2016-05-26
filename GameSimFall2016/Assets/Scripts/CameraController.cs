cc1f383f3426380a67d0c8682c9b625761ddee66
iirlas <iirlas@student.alamo.edu>
2016-05-26 04:46:35 -0500

    Added basic CameraController class.

---
+++ GameSimFall2016/Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Transform parent;
+
+    public float speed = 2.5f;
+
+    public Vector3 pivotPoint;
+    public Vector3 freeRoamLocalPosition;
+    public Vector3 FixedLocalPosition;
+
+
+    [HideInInspector]
+    public new Transform transform;
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LateUpdate()
+    {
+        if ( Game.getInstance().state == Game.State.FREEROAM )
+        {
+            float h = Input.GetAxis("Alt_Horizontal");
+            float v = Input.GetAxis("Alt_Vertical");
+
+            transform.localPosition = freeRoamLocalPosition;
+
+            if ( h != 0 || v != 0 )
+            {
+                // unify speed between mouse and joystick
+                h = (h > 0 ? 1 : -1) * speed;
+                v = (v > 0 ? 1 : -1) * speed;
+
+                parent.eulerAngles += new Vector3(0, h, 0);
+            }
+        }
+        else if ( Game.getInstance().state == Game.State.PUZZLE )
+        {
+            transform.localPosition = FixedLocalPosition;
+            parent.eulerAngles = Vector3.zero;
+        }
+
+
+        parent.position = target.position + pivotPoint;
+        transform.LookAt(target.position + pivotPoint, Vector3.up);
+    }
+
+}
