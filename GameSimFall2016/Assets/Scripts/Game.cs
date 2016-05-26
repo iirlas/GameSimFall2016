5ca42ae4abc9344df926cb99755c59853a88a9e8
iirlas <iirlas@student.alamo.edu>
2016-05-26 04:46:10 -0500

    Added Singleton game class.

---
+++ GameSimFall2016/Assets/Scripts/Game.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public enum State
+    {
+        FREEROAM,
+        PUZZLE,
+        CUTSCENE,
+        PAUSE,
+    }
+
+    static private Game instance;
+
+    public State state;
+
+    public static Game getInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<Game>();
+            if (instance == null)
+            {
+                GameObject singleton = new GameObject("Singleton.Game");
+                instance = singleton.AddComponent<Game>();
+                DontDestroyOnLoad(singleton);
+            }
+        }
+        return instance;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
