8c9488bd5c5620e64ff564b051da512e81006366
iirlas <iirlas@student.alamo.edu>
2016-06-05 00:19:45 -0500

    Added Player, base class for all playable characters. Added Girl, derived from Player for add functionality for the Girl protagonist. Revised GameSimFall2016.CSharp.csproj project file for the added classes

---
+++ GameSimFall2016/Assets/Scripts/Girl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Girl : Player {
+
+    private bool canJump = false;
+
+
+    // Use this for initialization
+    protected override void Start() 
+    {
+        base.Start();
+    }
+	
+	// Update is called once per frame
+    protected override void Update() 
+    {
+        base.Update();
+        int collidableLayerMask = 1 << 8;
+
+
+        bool isGrounded = false;
+        for (int angle = 0; angle < Mathf.PI * 2; angle++)// level with ground check
+        {
+            Vector3 direction = Quaternion.Euler(0, Mathf.Rad2Deg * angle, 0) * (Vector3.right / 2);
+            isGrounded |= Physics.Raycast(transform.position + direction, Vector3.down, 1.0f, collidableLayerMask);
+        }
+
+        Debug.DrawLine( transform.position, transform.position + transform.forward );
+
+        //can we move?
+        canMove = !Physics.Raycast(transform.position, transform.forward, transform.forward.magnitude, collidableLayerMask);
+        canMove &= isGrounded;
+        canRotate = isGrounded;
+
+        if (canMove)
+        {
+            //Are we on an edge?
+            if (!Physics.Raycast(transform.position, Vector3.down, 2.0f, collidableLayerMask))
+            {
+                if (canJump && isGrounded)//&& Input.GetButtonDown("Action"))
+                {
+                    //launches the player forward and up
+                    rigidbody.velocity = Vector3.zero;
+                    rigidbody.AddForce((transform.up + transform.forward) * movementSpeed, ForceMode.Impulse);
+                    canJump = false;
+                }
+            }
+            else
+            {
+                canJump = true;
+            }
+        }
+	}
+}
---
+++ GameSimFall2016/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float movementSpeed = 5.0f;
+    public float rotationSmoothSpeed = 10.0f;
+
+    new public Camera camera;
+
+    [HideInInspector]
+    public new Transform transform;
+    [HideInInspector]
+    public new Rigidbody rigidbody;
+
+    protected bool canRotate = true;
+    protected bool canMove = true;
+
+    private float mySmoothAngle;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    protected virtual void Update()
+    {
+        Vector3 cameraFoward = camera.transform.forward;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if ( h != 0 || v != 0 )
+        {
+            //            angle of joystick    angle of camera   
+            float angle = (Mathf.Atan2(h, v) + Mathf.Atan2(cameraFoward.x, cameraFoward.z)) * Mathf.Rad2Deg;
+
+            // smooths the rotation transition
+            if (canRotate)
+            {
+                mySmoothAngle = Mathf.LerpAngle(mySmoothAngle, angle, Time.deltaTime * rotationSmoothSpeed);
+                transform.localEulerAngles = new Vector3(0, mySmoothAngle, 0);
+            }
+            if ( canMove )
+            {
+                transform.position += transform.forward * movementSpeed * Time.deltaTime;
+            }
+
+        }
+	}
+}
