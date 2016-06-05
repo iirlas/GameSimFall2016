6939e3c9c07d60331d34e951ff9815e55f3d1985
iirlas <iirlas@student.alamo.edu>
2016-06-05 00:20:29 -0500

    Revised Game, for character switching functionality.

--- GameSimFall2016/Assets/Scripts/Game.cs
+++ GameSimFall2016/Assets/Scripts/Game.cs
@@ -14,6 +14,8 @@ public class Game : MonoBehaviour {
     static private Game instance;
 
     public State state;
+    public Player currentPlayer = null;
+    public Player[] players = new Player[4];
 
     public static Game getInstance()
     {
@@ -32,10 +34,77 @@ public class Game : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach ( GameObject player in players )
+        {
+            switch (player.name)
+            {
+            case "Girl":
+                this.players[0] = player.GetComponent<Player>();
+                break;
+            case "Bird":
+                this.players[1] = player.GetComponent<Player>();
+                break;
+            case "Rabbit":
+                this.players[2] = player.GetComponent<Player>();
+                break;
+            case "Cat":
+                this.players[3] = player.GetComponent<Player>();
+                break;
+            }
+        }
+        currentPlayer = this.players[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Player lastPlayer = currentPlayer;
+
+        float h = Input.GetAxis("Switch_Horizontal");
+        float v = Input.GetAxis("Switch_Vertical");
+
+        int switchIndex = ( h > 0 ? 1 : 
+                          ( h < 0 ? 3 :
+                          ( v > 0 ? 0 : 
+                          ( v < 0 ? 2 : -1 ))));
+
+
+
+
+        if ( switchIndex != -1 )
+        {
+            currentPlayer = players[switchIndex];
+        }
+
+        if (Input.GetButtonDown("Toggle"))
+        {
+            for (int i = 0, next = 0; i < players.Length; i++)
+            {
+                if (currentPlayer == players[i])
+                {
+                    do 
+                    {
+                        next = (i + 1) % players.Length;
+                    }
+                    while (!players[next] && next != i);
+
+                    currentPlayer = (players[next]);
+                    break;
+                }
+            }
+        }
+        foreach ( Player player in players )
+        {
+            if ( !!player )
+            {
+                bool isPlayer = (player == currentPlayer);
+                player.gameObject.SetActive(isPlayer);
+                if ( isPlayer && currentPlayer != lastPlayer )
+                {
+                    currentPlayer.transform.eulerAngles = lastPlayer.transform.eulerAngles;
+                    currentPlayer.transform.position = lastPlayer.transform.position;
+                }
+            }
+        }
 	}
 }
