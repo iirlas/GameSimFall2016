916da57d1de7e3d9201442df8f684f4275e28d54
iirlas <iirlas@student.alamo.edu>
2016-06-08 21:41:59 -0500

    Added Decay, destroys an object after an allotted time.

---
+++ GameSimFall2016/Assets/Scripts/Decay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decay : MonoBehaviour {
+
+    public float life = 100.0f;
+    public float decaySpeed = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        life -= decaySpeed * Time.deltaTime;
+        if ( life < 0 )
+        {
+            Destroy(gameObject);
+        }
+	}
+}
