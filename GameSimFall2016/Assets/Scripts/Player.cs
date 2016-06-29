f11fd533ce7e7162f08cbcdc68c1cdc612f53f4c
iirlas <iirlas@student.alamo.edu>
2016-06-29 14:38:38 -0500

    Revised Player, Made Player abstract.

--- GameSimFall2016/Assets/Scripts/Player.cs
+++ GameSimFall2016/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+abstract public class Player : MonoBehaviour {
 
     protected delegate void StateRunner();
 
