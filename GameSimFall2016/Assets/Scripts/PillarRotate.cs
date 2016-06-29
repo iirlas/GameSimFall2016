50ff9b105c87e3a24d3c9035bf77e3bcbc624b87
psipes <psipes@student.alamo.edu>
2016-06-29 16:45:13 -0500

    PAS turnable test level puzzle. Currently trigger has to be stepped on and off to work, locks after all match up.

---
+++ GameSimFall2016/Assets/Scripts/PillarRotate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarRotate : MonoBehaviour {
+
+    public GameObject pillarPiece;
+    public int numOfSides;
+    public static bool locked = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!locked)
+            {
+                pillarPiece.transform.Rotate(0, 0, 360 / numOfSides);
+            }
+
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
