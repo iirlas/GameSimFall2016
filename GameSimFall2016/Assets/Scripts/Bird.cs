ccff8792c158591b295437a8828cdb4d75d97b1c
iirlas <iirlas@student.alamo.edu>
2016-07-05 15:56:36 -0500

    Revised Bird, Added flying and shooting functionality WIP

--- GameSimFall2016/Assets/Scripts/Bird.cs
+++ GameSimFall2016/Assets/Scripts/Bird.cs
@@ -3,15 +3,80 @@ using System.Collections;
 
 public class Bird : Player {
 
+    enum State
+    {
+        FLY,
+    }
+
+    public GameObject rockPrefab;
+
+    private Vector3 myDirection;
+
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
+
+        addRunnable(State.FLY, runFlyState);
+
+        playerState = State.FLY;
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+        rigidbody.velocity = Vector3.zero;
+    }
+
+    void runFlyState ()
+    {
+        movePlayer();
+
+        if ( myDirection == Vector3.zero )
+        {
+            StartCoroutine(setFlyDirection(0.3f));
+        }
+
+        if ( Input.GetButtonDown("Attack") )
+        {
+            GameObject rock = Instantiate(rockPrefab, transform.position, transform.rotation) as GameObject;
+            Rigidbody rockBody = rock.GetComponent<Rigidbody>();
+            Physics.IgnoreCollision(rockBody.GetComponent<Collider>(), rigidbody.GetComponent<Collider>());
+            rockBody.AddForce(-transform.up * movementSpeed, ForceMode.Impulse);
+        }
+
+        if (Input.GetButton("Action"))
+        { 
+            transform.position += myDirection * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            myDirection = Vector3.zero;
+        }
+    }
+
+    IEnumerator setFlyDirection ( float pressDelay )
+    {
+        Timer timer = new Timer();
+        if (Input.GetButtonDown("Action"))
+        {
+            myDirection = transform.up;
+            timer.start();
+
+            yield return new WaitForEndOfFrame();// wait for input reset.
+            while (timer.elapsedTime() < pressDelay)
+            {
+                if (Input.GetButtonDown("Action"))
+                {
+                    myDirection = -transform.up;
+                    break;
+                }
+                yield return null;
+            }
+            yield break;
+        }
+        myDirection = Vector3.zero;
+        yield break;
     }
 }
