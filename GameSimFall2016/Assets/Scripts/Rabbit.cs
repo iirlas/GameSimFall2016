d2082f21fd8c4ddae89d944ba4f0de2cb09ccaa4
iirlas <iirlas@student.alamo.edu>
2016-07-06 15:53:36 -0500

    Revised Rabbit, added movement.

--- GameSimFall2016/Assets/Scripts/Rabbit.cs
+++ GameSimFall2016/Assets/Scripts/Rabbit.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class Rabbit : Player {
 
+    enum State
+    {
+        MOVE,
+        FALL,
+    }
+
 	// Use this for initialization
     protected override void Start()
     {
         base.Start();
+
+        addRunnable(State.MOVE, runMoveState);
+        addRunnable(State.FALL, runFallingState);
+
+        playerState = State.MOVE;
 	}
 	
 	// Update is called once per frame
@@ -14,4 +25,26 @@ public class Rabbit : Player {
     {
         base.Update();
 	}
+
+    void runMoveState ()
+    {
+        movePlayer();
+
+        if (!Physics.Raycast(transform.position + transform.forward, Vector3.down, 2.0f))//Are we on an edge then?
+        {
+            //launches the player forward and up
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce((transform.up + transform.forward) * movementSpeed, ForceMode.Impulse);
+            playerState = State.FALL;
+        }
+    }
+
+    void runFallingState ()
+    {
+        if (isGrounded())
+        {
+            playerState = State.MOVE;
+        }
+    }
+
 }
