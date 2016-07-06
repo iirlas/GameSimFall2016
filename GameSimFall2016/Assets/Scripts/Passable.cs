d6dc2c4a2b9e41bd52d7309c9feb0c3e8bc8d66c
iirlas <iirlas@student.alamo.edu>
2016-07-06 14:58:30 -0500

    Added Passable, Use for ignore collision of certain objects.

---
+++ GameSimFall2016/Assets/Scripts/Passable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Passable : MonoBehaviour {
+
+    [HideInInspector]
+    new public Collider collider;
+
+    public MonoBehaviour[] targets;
+
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<Collider>();
+
+        foreach ( MonoBehaviour target in targets )
+        {
+            Physics.IgnoreCollision(collider, target.GetComponent<Collider>());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
