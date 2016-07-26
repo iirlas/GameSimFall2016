807a7238a7d6187d35d5afd9435d95c3bd08370d
iirlas <iirlas@student.alamo.edu>
2016-07-26 03:02:04 -0500

    Revised Game, separated functionality into PlayerManager.

--- GameSimFall2016/Assets/Scripts/Game.cs
+++ GameSimFall2016/Assets/Scripts/Game.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Game : MonoBehaviour {
+public class Game : Singleton<Game> 
+{
 
     public enum GameState
     {
@@ -16,44 +17,14 @@ public class Game : MonoBehaviour {
         JOYSTICK
     }
 
-    static private Game instance;
-
     public GameState gameState;
 
     public InputState inputState;
 
-    [HideInInspector]
-    public Player currentPlayer = null;
-    public Player[] players = new Player[4];
-
-    public static Game getInstance()
-    {
-        if (instance == null)
-        {
-            instance = FindObjectOfType<Game>();
-            if (instance == null)
-            {
-                GameObject singleton = new GameObject("Game");
-                instance = singleton.AddComponent<Game>();
-                DontDestroyOnLoad(singleton);
-            }
-        }
-        return instance;
-    }
-
 	// Use this for initialization
-	void Start () 
+	override protected void Init () 
     {
         Application.targetFrameRate = 60;
-
-        players[0] = GameObject.FindObjectOfType<Girl>();
-        players[1] = GameObject.FindObjectOfType<Bird>();
-        players[2] = GameObject.FindObjectOfType<Rabbit>();
-        players[3] = GameObject.FindObjectOfType<Cat>();
-
-        currentPlayer = this.players[0];
-
-        StartCoroutine(activeSet());
 	}
 	
 	// Update is called once per frame
@@ -80,80 +51,7 @@ public class Game : MonoBehaviour {
             if ( isJoystick )
             {
                 inputState = InputState.JOYSTICK;
-
             }
-        }
-    }
-
-    IEnumerator activeSet ()
-    {
-        while ( true )
-        {
-            float h = Input.GetAxis("Switch_Horizontal");
-            float v = Input.GetAxis("Switch_Vertical");
-
-            int switchIndex = (h > 0 ? 1 :
-                              (h < 0 ? 3 :
-                              (v > 0 ? 0 :
-                              (v < 0 ? 2 : -1))));
-
-
-            if (switchIndex != -1 && players[switchIndex])
-            {
-                currentPlayer = players[switchIndex];
-                yield return null;
-            }
-
-            if (Input.GetButtonDown("Toggle"))
-            {
-                for (int i = 0, next = 0; i < players.Length; i++)
-                {
-                    if (currentPlayer == players[i])
-                    {
-                        next = i;
-                        do
-                        {
-                            next = (next + 1) % players.Length;
-                        } while (!players[next] && next != i);
-
-                        currentPlayer = players[next];
-                        yield return null;
-                        break;
-                    }
-                }
-            }
-
-            if ( currentPlayer == players[0] )
-            {
-                for ( int index = 0; index < players.Length; index++ )
-                {
-                    Player player = players[index];
-                    if (player != null && currentPlayer != player &&
-                        Vector3.Distance(player.transform.position, currentPlayer.transform.position) < 5.0f)
-                    {
-                        Player target = null;
-                        for (int j = index, breakCount = 0; target == null && breakCount < players.Length; 
-                            j = (--j >= 0 ? j : j + players.Length), breakCount++ )
-                        {
-                            if (players[j] != null && players[j] != player && 
-                                Vector3.Distance(players[j].transform.position, currentPlayer.transform.position) < 5.0f)
-                            {
-                                target = players[j];
-                            }
-                        }
-
-                        if ( target != null )
-                        {
-                            player.smoothRotateTowards(target.transform.eulerAngles, Time.deltaTime * player.rotationSmoothSpeed);
-                            player.transform.position = Vector3.MoveTowards(player.transform.position, 
-                                                                            target.transform.position - 
-                                                                                (player.transform.forward), 
-                                                                            player.movementSpeed * Time.deltaTime   );
-                        }
-                    }
-                }
-            }
-            yield return null;
         }
     }
 }
