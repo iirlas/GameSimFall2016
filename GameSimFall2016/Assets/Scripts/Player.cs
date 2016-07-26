deac7643b9634c030396bae3ab3cabd914a34fe5
iirlas <iirlas@student.alamo.edu>
2016-07-26 02:57:32 -0500

    Revised Player, Added Constructor, Revised basic properties, added sizeOffset parameter to isGrounded.

--- GameSimFall2016/Assets/Scripts/Player.cs
+++ GameSimFall2016/Assets/Scripts/Player.cs
@@ -3,68 +3,101 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-abstract public class Player : MonoBehaviour {
+abstract public class Player : MonoBehaviour
+{
 
     protected delegate void StateRunner();
+
+    private Transform myTransform;
+    private Rigidbody myRigidbody;
+    private Collider myCollider;
 
     public float movementSpeed = 5.0f;
     public float rotationSmoothSpeed = 10.0f;
 
-
     new public Camera camera;
 
     [HideInInspector]
-    public new Transform transform;
-    [HideInInspector]
-    public new Rigidbody rigidbody;
-    [HideInInspector]
-    public new Collider collider;
+    public new Transform transform
+    {
+        get
+        {
+            if (myTransform == null)
+            {
+                myTransform = base.transform;
+            }
+            return myTransform;
+        }
+    }
 
-    public Enum playerState;
-    
+    [HideInInspector]
+    public new Rigidbody rigidbody
+    {
+        get
+        {
+            if (myRigidbody == null)
+            {
+                myRigidbody = GetComponent<Rigidbody>();
+            }
+            return myRigidbody;
+        }
+    }
+
+    [HideInInspector]
+    public new Collider collider
+    {
+        get
+        {
+            if (myCollider == null)
+            {
+                myCollider = GetComponent<Collider>();
+            }
+            return myCollider;
+        }
+    }
+    public Enum playerState { get; protected set; }
+
     private Dictionary<Enum, StateRunner> myStates;
 
-	// Use this for initialization
-	protected virtual void Start () {
-        transform = GetComponent<Transform>();
-        rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
-        myStates = new Dictionary<Enum, StateRunner>();
-	}
-	
-	// Update is called once per frame
-    protected virtual void Update()
+    //constructor
+    public Player()
     {
-        if ( Game.getInstance().currentPlayer == this )
+        myStates = new Dictionary<Enum, StateRunner>();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (PlayerManager.getInstance().currentPlayer == this)
         {
             myStates[playerState]();
         }
-	}
+    }
 
-    protected void addRunnable ( Enum state, StateRunner stateRunner )
+    protected void addRunnable(Enum state, StateRunner stateRunner)
     {
         myStates.Add(state, stateRunner);
     }
 
-    protected void movePlayer ()
+    protected void movePlayer()
     {
         Vector3 cameraFoward = camera.transform.forward;
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        if ( h != 0 || v != 0 )
+        if (h != 0 || v != 0)
         {
             //            angle of joystick  + angle of camera
             float angle = (Mathf.Atan2(h, v) + Mathf.Atan2(cameraFoward.x, cameraFoward.z)) * Mathf.Rad2Deg;
 
             // smooths the rotation transition
             transform.position += transform.forward * movementSpeed * Time.deltaTime;
-            smoothRotateTowards( 0, angle, 0, Time.deltaTime * rotationSmoothSpeed );
+            smoothRotateTowards(0, angle, 0, Time.deltaTime * rotationSmoothSpeed);
         }
     }
 
-    public void smoothRotateTowards ( float x, float y, float z, float speed )
+    public void smoothRotateTowards(float x, float y, float z, float speed)
     {
         Vector3 angle = transform.localEulerAngles;
         angle.x = Mathf.LerpAngle(angle.x, x, speed);
@@ -73,7 +106,7 @@ abstract public class Player : MonoBehaviour {
         transform.localEulerAngles = angle;
     }
 
-    public void smoothRotateTowards( Vector3 target, float speed)
+    public void smoothRotateTowards(Vector3 target, float speed)
     {
         Vector3 angle = transform.localEulerAngles;
         angle.x = Mathf.LerpAngle(angle.x, target.x, speed);
@@ -82,28 +115,28 @@ abstract public class Player : MonoBehaviour {
         transform.localEulerAngles = angle;
     }
 
-    protected bool isGrounded(float? distance = null, int step = 10)
+    protected bool isGrounded(float sizeOffset = 0, int step = 10, float? distance = null)
     {
         if (distance == null)
         {
             distance = collider.bounds.extents.y * 1.25f;
         }
 
-        float width = collider.bounds.size.x;
-        float depth = collider.bounds.size.z;
+        float width = collider.bounds.size.x + sizeOffset;
+        float depth = collider.bounds.size.z + sizeOffset;
 
         float xSteps = width / step;
         float zSteps = depth / step;
 
         // Are we level with the ground?
-        for (float x = 0; Mathf.Round(x) < width; x += xSteps )
+        for (float x = -sizeOffset; Mathf.Round(x) < width; x += xSteps)
         {
-            for (float z = 0; Mathf.Round(z) < depth; z += zSteps )
+            for (float z = -sizeOffset; Mathf.Round(z) < depth; z += zSteps)
             {
                 Vector3 origin = collider.bounds.min + (Vector3.right * x) + (Vector3.forward * z);
                 origin.y = transform.position.y;
 
-                Debug.DrawRay(origin, Vector3.down * (float)distance );
+                Debug.DrawRay(origin, Vector3.down * (float)distance);
                 if (Physics.Raycast(origin, Vector3.down, (float)distance))
                 {
                     return true;
