2ea7de49e09b2ac27d3320fc52844f99016fedd8
iirlas <iirlas@student.alamo.edu>
2016-07-27 16:28:57 -0500

    Added MovementPauseEvent.

---
+++ GameSimFall2016/Assets/Scripts/MovementPauseEvent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementPauseEvent : MonoBehaviour {
+
+    public float pauseTime = 1.0f;
+
+    private Timer myTimer = new Timer();
+    private Movement myMovement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( myTimer.elapsedTime() >= pauseTime )
+        {
+            myMovement.canMove = true;
+            myTimer.stop();
+        }
+	}
+
+    void OnMovementStart ( Movement movement )
+    {
+        myMovement = movement;
+    }
+
+    void OnMovementEnd ( )
+    {
+
+    }
+
+    void OnNodeReached ( )
+    {
+        myMovement.canMove = false;
+        myTimer.start();
+    }
+}
