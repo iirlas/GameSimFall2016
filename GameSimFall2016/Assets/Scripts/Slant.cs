b9cf8592ee7f6ebf8be311eb2c1bcb87fedb5343
iirlas <iirlas@student.alamo.edu>
2016-08-03 22:24:16 -0500

    Added Tag. Added Slant.

---
+++ GameSimFall2016/Assets/Scripts/Slant.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slant : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnEvent (BasicTrigger instigator) 
+    {
+        switch(instigator.message)
+        {
+        case "UP":
+            transform.position += Vector3.up;
+            break;
+        case "DOWN":
+            transform.position += Vector3.down;
+            break;
+        }
+	}
+}
---
+++ GameSimFall2016/Assets/Scripts/Tag.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[System.Serializable]
+public struct Tag
+{
+    public string value;
+
+    public Tag(string tag)
+    {
+        value = tag;
+    }
+
+    public static implicit operator Tag(string tag)
+    {
+        return new Tag(tag);
+    }
+
+    public static implicit operator string(Tag tag)
+    {
+        return tag.value;
+    }
+}
+
+[CustomPropertyDrawer(typeof(Tag))]
+public class TagDrawer : PropertyDrawer
+{
+    
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        label = EditorGUI.BeginProperty(position, label, property);
+
+        string tag = property.FindPropertyRelative("value").stringValue;
+
+        tag = EditorGUI.TagField(position, label, tag);
+        property.FindPropertyRelative("value").stringValue = tag;
+
+        EditorGUI.EndProperty();
+    }
+}
