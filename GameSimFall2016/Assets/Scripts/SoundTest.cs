44425c82355916eda8f4db039fd9c4612b0dfa4f
jmolina112 <jacintomolina@rocketmail.com>
2016-08-24 12:59:52 -0500

    Added simple sound Script

---
+++ GameSimFall2016/Assets/Scripts/SoundTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTest : MonoBehaviour {
+
+	public AudioSource soundEffect;
+
+	void Start () {
+
+	}
+
+
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player" && soundEffect.isPlaying == false) {
+			soundEffect.Play ();
+		}
+			
+	}
+
+	public void OnTriggerStay(Collider col)
+	{
+		if (col.gameObject.tag == "Player" && soundEffect.isPlaying == true) {
+			soundEffect.loop = true;
+		}
+	}
+	public void OnTriggerExit(Collider col)
+	{
+		if (soundEffect.loop == true) {
+			soundEffect.loop = false;
+		}
+	}
+}
