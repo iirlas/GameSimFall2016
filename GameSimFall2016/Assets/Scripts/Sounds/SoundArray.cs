9b25e76cd7d58eaba51f4e009b79d5ccf86f35a0
jmolina112 <jacintomolina@rocketmail.com>
2016-08-26 13:16:31 -0500

    Different Scripts for Audio

---
+++ GameSimFall2016/Assets/Scripts/Sounds/SoundArray.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundArray : MonoBehaviour {
+
+	AudioSource soundSource;
+	public AudioClip[] soundArray;
+	public AudioClip sound1;
+	public AudioClip sound2;
+	public AudioClip sound3;
+	public float timeSec = 2f;
+	float delay;
+	int randomNumber;
+
+
+	// saving sound clips into an array of sounds that can randomize different sounds for the enemy.
+	void Start () 
+	{
+		soundSource = GetComponent<AudioSource> ();
+		randomNumber = Random.Range (0, 3);
+		Debug.Log (randomNumber);
+		soundArray = new AudioClip[3];
+		soundArray [0] = sound1;
+		soundArray [1] = sound2;
+		soundArray [2] = sound3;
+		soundSource.clip = soundArray [randomNumber];
+		AudioSource.PlayClipAtPoint(soundArray[randomNumber],this.gameObject.GetComponent<Transform>().position);
+		delay = timeSec;
+	}
+		
+
+	void Update () 
+	{
+		//soundEffect.PlayDelayed (1.0f);
+		// get the constant update position of the gameobject and use it to move the sound around with it.
+		if (soundSource.isPlaying == false && timeSec <= 0.0f) 
+		{
+			randomNumber = Random.Range (0, 3);
+			Debug.Log (randomNumber);
+			soundSource.clip = soundArray [randomNumber];
+			//soundSource.Play ();
+			AudioSource.PlayClipAtPoint(soundArray[randomNumber],this.gameObject.GetComponent<Transform>().position);
+			timeSec = delay;
+		}
+
+	
+		if (soundSource.isPlaying == false) {
+			timeSec -= Time.deltaTime;
+		}
+	}
+}
---
+++ GameSimFall2016/Assets/Scripts/Sounds/SoundDelay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDelay : MonoBehaviour {
+
+	public AudioSource soundEffect;
+	public float timeSec = 2f;
+	public float delay;
+
+	void Start ()
+	{
+		soundEffect.Play ();
+		delay = timeSec;
+	}
+
+	void Update () 
+	{
+		//soundEffect.PlayDelayed (1.0f);	
+		if (soundEffect.isPlaying == false && timeSec <= 0.0f) 
+		{
+			soundEffect.Play ();
+			timeSec = delay;
+		}
+		if (soundEffect.isPlaying == false) {
+			timeSec -= Time.deltaTime;
+		}
+	}
+}
