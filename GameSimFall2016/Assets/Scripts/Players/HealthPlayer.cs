81a01a3a90dfa57d8901ee6ca1bae2437ff18fd7
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-08-29 15:54:51 -0500

    NRC:  Added health values and damage function to HealthPlayer.cs

--- GameSimFall2016/Assets/Scripts/Players/HealthPlayer.cs
+++ GameSimFall2016/Assets/Scripts/Players/HealthPlayer.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-//========================================================================================================
+﻿//========================================================================================================
 //                                              HEALTH PLAYER
 //  This script has a slider that controls the health of the player. Currently a static variable (healthChange)
 //  can be accessed to control change in health (negative numbers for damage, positive for healing).
@@ -15,48 +12,63 @@ using UnityEngine.UI;
 // 
 //
 //========================================================================================================
-public class HealthPlayer : MonoBehaviour {
 
-    public Slider healthBar; //hook the healthbar to this slider spot
-    public int healthMax;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
-    public static int healthChange;
+public class HealthPlayer : MonoBehaviour
+{
 
-    private int healthCurrent;
-    
+   public Slider healthBar; //hook the healthbar to this slider spot
+   public int healthMax;
 
-	// initialises health change, healthCurrent
-    // sets maxValue of slider to healthMax
-    // sets minValue to 0
-	void Start () {
+   public static int healthChange;
 
-        healthChange = 0;
-        healthCurrent = healthMax;
-        healthBar.maxValue = healthMax;
-        healthBar.minValue = 0;
+   public int healthCurrent;
 
-	
-	}
-	
+   bool isDead = false;
 
-    // This updates the health and checks if she should be dead.
-    // if dead, debug log tells you she's dead.
-	void Update () {
-        if (healthChange != 0)
-        {
-            healthCurrent += healthChange;
-            if (healthCurrent > 1)
-            {
-                healthBar.value = healthBar.minValue;
-                Debug.Log("Player is Dead.");
-            } else
-            {
-                healthBar.value = healthCurrent;
-            }
-            
+   //==========================================================================
+   // initialises health change, healthCurrent
+   // sets maxValue of slider to healthMax
+   // sets minValue to 0
+   void Start()
+   {
 
-        }
+      //this.healthChange = 0;
+      this.healthCurrent = healthMax;
+      this.healthBar.maxValue = healthMax;
+      this.healthBar.minValue = 0;
 
-	
-	}
+   }
+
+   //==========================================================================
+   // This updates the health and checks if she should be dead.
+   // if dead, debug log tells you she's dead.
+   void Update()
+   {
+      if (!isDead)
+      {
+         if (this.healthCurrent < 1)
+         {
+            this.healthBar.value = healthBar.minValue;
+            Debug.Log("Player is Dead.");
+            isDead = true;
+         }
+         else
+         {
+            this.healthBar.value = this.healthCurrent;
+         }
+      }
+   }
+
+   //==========================================================================
+   // Modify the health of the player, value can be positive or negative.
+   // If the value is positive, the player gains health.
+   // If the value is negative, the player loses health.
+   public void modifyHealth(int value)
+   {
+      this.healthCurrent += value;
+   }
 }
