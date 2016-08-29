eed946d061d7787da95e271b3231602536f6fede
psipes <psipes@student.alamo.edu>
2016-08-29 17:34:27 -0500

    PAS maybe level 6

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/KeyOpen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyOpen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEvent(BasicTrigger trigger)
+    {
+        if (trigger.message == "grabKey")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
