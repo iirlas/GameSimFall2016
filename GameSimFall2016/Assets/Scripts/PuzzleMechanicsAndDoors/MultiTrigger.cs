a371e1c29d1e6e43998447610176d048a3d97633
iirlas <iirlas@student.alamo.edu>
2016-08-31 11:32:33 -0500

    Added MultiTrigger.

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/MultiTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiTrigger : MonoBehaviour {
+
+    private int triggerCount = 0;
+
+    public MonoBehaviour effected;
+    public string message;
+    public BasicTrigger[] triggers;
+
+    void OnEvent ( BasicTrigger trigger )
+    {
+        if (trigger.message != message)
+        {
+            throw new System.Exception("Message for [" + trigger.gameObject.name + "] does not match [" + name + "]");
+        }
+        if (++triggerCount == triggers.Length)
+        {
+            effected.SendMessage("OnEvent", trigger);
+        }
+    }
+
+    void OnEventEnd ( BasicTrigger trigger )
+    {
+        if (trigger.message != message)
+        {
+            throw new System.Exception("Message for [" + trigger.gameObject.name + "] does not match [" + name + "]");
+        }
+        if (--triggerCount == (triggers.Length - 1))
+        {
+            effected.SendMessage("OnEventEnd", trigger);
+        }
+    }
+
+}
