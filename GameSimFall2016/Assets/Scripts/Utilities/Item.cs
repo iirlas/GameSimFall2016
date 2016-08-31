6531ec1f412004d44f02a28a0a8b9c5dc1822993
iirlas <iirlas@student.alamo.edu>
2016-08-31 11:31:26 -0500

    Added Item.

---
+++ GameSimFall2016/Assets/Scripts/Utilities/Item.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Item : MonoBehaviour {
+
+    private Collider myCollider;
+
+    [HideInInspector]
+    new public Collider collider
+    {
+        get
+        {
+            if (myCollider == null)
+            {
+                myCollider = GetComponent<Collider>();
+            }
+            return myCollider;
+        }
+    }
+
+    public Collider[] targets;
+
+    public void OnCollisionEnter (Collision collision)
+    {
+        if (collision.transform.tag == "Player" && targets.Contains(collision.collider))
+        {
+            PlayerManager.getInstance().items.Add(transform.name, this);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnTriggerEnter (Collider other)
+    {
+        if (other.transform.tag == "Player" && targets.Contains(other))
+        {
+            PlayerManager.getInstance().items.Add(transform.name, this);
+            gameObject.SetActive(false);
+        }
+    }
+}
