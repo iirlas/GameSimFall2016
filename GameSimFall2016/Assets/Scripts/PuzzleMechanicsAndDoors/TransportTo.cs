0602c9787a8c63e80aa2f43cd90a5be951b2d065
psipes <psipes@student.alamo.edu>
2016-09-09 17:05:55 -0500

    PAS transportroom in.

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/TransportTo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TransportTo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnEvent(BasicTrigger trigger)
+    {
+        if (trigger.message == "nextLevel")
+        {
+            SceneManager.LoadScene("TestOverworld");
+        }
+    }
+}
