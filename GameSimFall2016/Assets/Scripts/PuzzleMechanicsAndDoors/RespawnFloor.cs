35bb6d2fd73215dd684a37f44cc5aefcdb08123a
psipes <psipes@student.alamo.edu>
2016-09-09 13:46:18 -0500

    PAS fixed scripts and prefabs

--- GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/RespawnFloor.cs
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/RespawnFloor.cs
@@ -1,25 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//RespawnFloor code
+//Creator: Pat Sipes
+//Version 2.0
+//Description: This code works in conjunction with the basicTrigger script. 
+//It is placed on the empty object to be respawned to.
+//Death floor must have the basic trigger script send the message "respawn" and be repeatable.
+
 public class RespawnFloor : MonoBehaviour {
 
-    public GameObject player;
+    //public GameObject player;
+    private GameObject[] playerUnits; //to collect the player units together
+     
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        playerUnits = GameObject.FindGameObjectsWithTag("Player"); //populate array
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void OnEvent(BasicTrigger trigger)
+    void OnEvent(BasicTrigger trigger) //Basic Trigger script
     {
+        
         if (trigger.message == "respawn")
         {
-            player.transform.position = transform.position;
+            
+
+            foreach (GameObject unit in playerUnits)
+            {
+                if (trigger.activator.Equals(unit))
+                {
+                    unit.transform.position = transform.position; //respawn to designated respawn block
+                }
+            }
+            //player.transform.position = transform.position;
         }
     }
 }
