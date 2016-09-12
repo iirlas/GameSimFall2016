d1cde7d66a22564d47536718e7ba52b917482e49
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-09-12 14:43:50 -0500

    NRC:  Campfire.cs added

---
+++ GameSimFall2016/Assets/Scripts/Campfire.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Campfire : MonoBehaviour
+{
+
+   private Transform respawnPoint;
+
+   //==========================================================================
+   // Use this for initialization
+   void Start()
+   {
+      respawnPoint = this.GetComponentInChildren<Transform>();
+   }
+
+   //==========================================================================
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   //==========================================================================
+   // When the player enters the trigger zone, set to current spawn point
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.name.Equals("Kira"))
+      {
+         GameObject.FindGameObjectWithTag("HealthManager").GetComponent<HealthPlayer>().campfireRespawn = this.respawnPoint;
+      }
+   }
+
+   //==========================================================================
+   // While the player stays in the trigger zone, heal the player.
+   void OnTriggerStay(Collider other)
+   {
+      if (other.gameObject.name.Equals("Kira"))
+      {
+         healPlayer();
+      }
+   }
+
+   //==========================================================================
+   // heals the player
+   void healPlayer()
+   {
+      GameObject.FindGameObjectWithTag("HealthManager").GetComponent<HealthPlayer>().modifyHealth(1);
+   }
+}
