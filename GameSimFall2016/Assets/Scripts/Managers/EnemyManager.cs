a0f31d9158ce77888d50fc1b148030e33c55b2a7
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-09-12 20:22:50 -0500

    NRC:  Fixed line ending warning, for future reference:  Edit > Advanced > Format Document

--- GameSimFall2016/Assets/Scripts/Managers/EnemyManager.cs
+++ GameSimFall2016/Assets/Scripts/Managers/EnemyManager.cs
@@ -20,7 +20,7 @@ public class EnemyManager : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      if (Input.GetKeyDown(KeyCode.Delete))
+      if (Input.GetKeyDown(KeyCode.Delete) && killKeyEnabled)
       {
          killAllEnemies();
       }
