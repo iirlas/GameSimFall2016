c87c230d0a1bba10fee0e9eb008449cf6585cab7
psipes <psipes@student.alamo.edu>
2016-09-21 16:45:55 -0500

    PAS added prefab puzzle thing for tutorial. yes?

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/MovePanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePanel : MonoBehaviour {
+
+    public GameObject endPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnEvent(BasicTrigger trigger)
+    {
+        if (trigger.message == "movePanel")
+        {
+            this.gameObject.transform.position = endPoint.transform.position;
+
+        }
+    }
+}
