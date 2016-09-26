d463b8e51428a144c5fe817cc71969cfa05af516
ninjawithglasses <legendarysparda09@gmail.com>
2016-09-26 12:19:43 -0500

    JCG: Light Collider

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/LightCollider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class LightCollider : MonoBehaviour {
+
+    private Light light;
+    public LayerMask layer;
+    private Collider[] myHits;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Collider[] hits = Physics.OverlapSphere(transform.position, light.range, layer);
+        if (myHits != null)
+        {
+            foreach (var hit in myHits)
+            {
+                if (!hits.Contains(hit))
+                {
+                    hit.transform.SendMessage("OnActionEnd", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+        foreach (var hit in hits)
+        {
+            hit.transform.SendMessage("OnAction", SendMessageOptions.DontRequireReceiver);
+        }
+        myHits = hits;
+	}
+}
