268835afb7d13ee0e74e45abab465874f9470f8c
psipes <psipes@student.alamo.edu>
2016-09-26 18:41:45 -0500

    PAS scripts ftw

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/CheckLights.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckLights : MonoBehaviour {
+
+    public GameObject exitDoor;
+    public Light[] desiredLights; 
+
+    private int count;
+    
+
+	// Use this for initialization
+	void Start () {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Light myLight in desiredLights)
+        {
+            if (myLight.intensity > 0)
+                count++;
+        }
+
+        if (count == desiredLights.Length)
+        {
+            Debug.Log("countMet");
+            exitDoor.SetActive(false);
+
+        }
+        else
+        {
+            count = 0;
+        }
+	
+	}
+}
---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/TargetLightActivation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetLightActivation : MonoBehaviour {
+
+    public Light[] puzzleBoxes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="Projectile" || other.tag == "Player")
+        {
+
+            Debug.Log("hitPanel");
+            foreach (Light boxLight in puzzleBoxes)
+            {
+                Debug.Log(boxLight.intensity);
+                if (boxLight.intensity < 1)
+                    boxLight.intensity = 1;
+                else
+
+                    boxLight.intensity = 0;
+
+                Debug.Log(boxLight.intensity);
+            }
+        }
+    }
+}
