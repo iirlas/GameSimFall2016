c08c186e2d48ae2cf7de5497e9054c1376f8fa16
psipes <psipes@student.alamo.edu>
2016-09-28 13:38:18 -0500

    PAS added a cursor Lock script that confines the cursor to the window, then, on escape, it exits the application and returns the mouse to you. THIS SHOULD BE EDITED SO THAT THE FUNCTIONALITY IS BETTER.

---
+++ GameSimFall2016/Assets/Scripts/Utilities/CursorLock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLock : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Application.Quit(); // Quits the game
+        }
+    }
+}
