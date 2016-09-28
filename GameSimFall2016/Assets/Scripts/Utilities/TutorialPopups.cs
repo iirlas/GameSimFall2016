184d058b221075424a4a9283da15782ea415ef1c
ninjawithglasses <legendarysparda09@gmail.com>
2016-09-28 11:55:55 -0500

    JCG: TutorialWaterRoom finished

--- GameSimFall2016/Assets/Scripts/Utilities/TutorialPopups.cs
+++ GameSimFall2016/Assets/Scripts/Utilities/TutorialPopups.cs
@@ -25,13 +25,21 @@ public class TutorialPopups : MonoBehaviour {
         {
             tutText.text = "It looks much safer in the light. I should head to a torch.";
         }
+        else if (trigger.message == "TutFearMeter")
+        {
+            tutText.text = "If I stay in the light I feel less afraid...\n Watch your Fear Meter. If it hits 100, you start taking physical damage.";
+        }
         else if (trigger.message == "Tut2")
         {
             tutText.text = "What's a bunny doing here? I should protect it! \n Press right click to lock on, left click to shoot.";
         }
+        else if (trigger.message == "TutPanelUp")
+        {
+            tutText.text = "It doesn't look like I can jump that. Maybe Bunny can! \n Press Q to switch between bunny and Kira, \npress SPACE as bunny to Jump";
+        }
         else if (trigger.message == "Tut3")
         {
-            tutText.text = "There are two plates. \nI wonder if I step on one and send bunny to the other if the door will open? \n Press Q to switch between bunny and Kira";
+            tutText.text = "There are two plates. \nI wonder if I step on one and send bunny to the other if the door will open?";
         }
         else if (trigger.message == "Tut4")
         {
