17cf1de80f4f5e7e16ef83c4f720cd4d21dcd6ff
psipes <psipes@student.alamo.edu>
2016-09-30 18:53:10 -0500

    PAS Damage over time for Fire room.

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/DamageOnCollision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnCollision : MonoBehaviour {
+    private bool takeDamage;
+	// Use this for initialization
+	void Start () {
+        takeDamage = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (takeDamage)
+        {
+            Invoke("takeFireDamage", 1f);
+
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.name.Equals("Kira"))
+        {
+            takeDamage = true;
+        }
+
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        takeDamage = false;
+    }
+
+
+    void takeFireDamage()
+    {
+        GameObject.FindGameObjectWithTag("HealthManager").GetComponent<HealthPlayer>().modifyHealth(-5);
+    }
+}
