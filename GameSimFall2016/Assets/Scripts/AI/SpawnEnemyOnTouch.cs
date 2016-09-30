1add1c33e7a679848371ac621eec5b537f6a700d
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-09-30 17:48:13 -0500

    NRC: I did a thingy

---
+++ GameSimFall2016/Assets/Scripts/AI/SpawnEnemyOnTouch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnEnemyOnTouch : MonoBehaviour
+{
+
+    public GameObject theEnemy;
+    public GameObject spawnPOS;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Kira"))
+        {
+            GameObject tempEnemy = Instantiate(theEnemy, spawnPOS.transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+}
