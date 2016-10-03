21d7e5b8cf4be4e99123819aca8e881120af9860
ninjawithglasses <legendarysparda09@gmail.com>
2016-10-03 13:59:53 -0500

    JCG: Added nathan's super death script

---
+++ GameSimFall2016/Assets/Scripts/Utilities/SuperDeathFloor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperDeathFloor : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Kira"))
+        {
+            GameObject.FindGameObjectWithTag("HealthManager").GetComponent<HealthPlayer>().modifyHealth(-10000);
+        }
+        else if (other.gameObject.tag.Equals("Player"))
+        {
+            other.transform.position = GameObject.FindGameObjectWithTag("HealthManager").GetComponent<HealthPlayer>().campfireRespawn.position;
+        }
+    }
+}
