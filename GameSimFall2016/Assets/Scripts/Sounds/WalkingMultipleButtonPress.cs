12a12dc51cefca73bc3545621209fcf9d326a67f
jmolina112 <jacintomolina@rocketmail.com>
2016-10-03 13:30:07 -0500

    walking sound Script with multiple button pressed

---
+++ GameSimFall2016/Assets/Scripts/Sounds/WalkingMultipleButtonPress.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingMultipleButtonPress : MonoBehaviour {
+
+	AudioSource walkingAudio;
+	void Start () 
+	{
+		walkingAudio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W) == true && Input.GetKey (KeyCode.D) == false && Input.GetKey (KeyCode.A) == false && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+
+		} else if (Input.GetKey (KeyCode.W) == true && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == false && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		} else if (Input.GetKey (KeyCode.W) == true && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		} else if (Input.GetKey (KeyCode.W) == true && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == true) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		} else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == false && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		} else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		}else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == true && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == true) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		}else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == false && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == false) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		}else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == false && Input.GetKey (KeyCode.A) == true && Input.GetKey (KeyCode.S) == true) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		}else if (Input.GetKey (KeyCode.W) == false && Input.GetKey (KeyCode.D) == false && Input.GetKey (KeyCode.A) == false && Input.GetKey (KeyCode.S) == true) {
+			if (walkingAudio.isPlaying == false) {
+
+				walkingAudio.Play ();
+			}
+
+		}else if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D)|| Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.S))
+		{
+			walkingAudio.Stop ();
+		}
+	}
+}
