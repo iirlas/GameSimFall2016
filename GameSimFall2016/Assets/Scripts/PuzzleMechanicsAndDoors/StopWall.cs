9bcdd4743977e8236c16a7ac148a6cbc947d33d2
ToonTalker <ToonTalker@gmail.com>
2016-10-03 15:05:41 -0500

    PAS stopwall script addded

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/StopWall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopWall : MonoBehaviour {
+    bool endMove;
+    [Tooltip("The object that starts the animation")]
+    public GameObject switchWheel;
+    public GameObject invisiWall;
+    [Tooltip("Each Wall to begin Movement on BASIC TRIGGER")]
+    public GameObject[] walls;
+	// Use this for initialization
+	void Start () {
+        endMove = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (endMove)
+        {
+
+            foreach (GameObject wall in walls)
+            {
+                Animator myanim = wall.GetComponent<Animator>();
+                myanim.SetBool("puzzleSolved", true);
+            }
+
+        }
+	
+	}
+
+    void OnEvent(BasicTrigger trigger)
+    {
+
+        if (trigger.message == "stopWalls")
+        {
+            endMove = true;
+            Destroy(switchWheel.GetComponent<MoveWall>());
+            Destroy(invisiWall);
+        }
+    }
+}
