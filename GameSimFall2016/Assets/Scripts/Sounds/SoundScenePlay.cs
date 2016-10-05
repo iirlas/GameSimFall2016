141629b2baf3e74c5754411e158238043f474357
jmolina112 <jacintomolina@rocketmail.com>
2016-10-05 17:50:36 -0500

    Adding script to switch music based on the name of the scenes. this allows me to continue playing the same song between scenes for the same level.

---
+++ GameSimFall2016/Assets/Scripts/Sounds/SoundScenePlay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class SoundScenePlay : MonoBehaviour {
+
+	public AudioSource levelMusic;
+
+
+	public static AudioClip[] gameMusic = new AudioClip[2];
+	public AudioClip outsideMusic;
+	public AudioClip insideCave;
+	string nameTest = "";
+	bool OnlyOnce = false;
+
+
+
+	void Awake () 
+	{
+		this.levelMusic.Stop ();
+		this.levelMusic.clip = insideCave;
+		this.levelMusic.Play ();
+		/*for (int soundIndex = 0; soundIndex < gameMusic.Length; soundIndex++) {
+			gameMusic[soundIndex] = this.o
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (nameTest != EditorSceneManager.GetActiveScene ().name) {
+			nameTest = EditorSceneManager.GetActiveScene ().name;
+			Debug.Log ("The name of the Scene -" + nameTest + "-");
+			OnlyOnce = false;
+		}
+
+		// changes the song of the player to play another one based on the name of the scene.
+		if (this.nameTest.Equals("TestOverworld") && OnlyOnce == false) {
+			this.levelMusic.Stop ();
+			this.levelMusic.clip = outsideMusic;
+			this.levelMusic.Play ();
+			Debug.Log ("made it into the switch method");
+			OnlyOnce = true;
+		}
+
+
+		DontDestroyOnLoad (levelMusic);
+
+	}
+}
