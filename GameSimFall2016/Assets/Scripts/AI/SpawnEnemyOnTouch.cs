459ac13447e05b45a402702acb89cc178c03f955
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-10-05 12:40:02 -0500

    NRC:  Fixed SpawnEnemyOnTouch.cs warnings

--- GameSimFall2016/Assets/Scripts/AI/SpawnEnemyOnTouch.cs
+++ GameSimFall2016/Assets/Scripts/AI/SpawnEnemyOnTouch.cs
@@ -4,24 +4,24 @@ using System.Collections;
 public class SpawnEnemyOnTouch : MonoBehaviour
 {
 
-    public GameObject theEnemy;
-    public GameObject spawnPOS;
+   public GameObject theEnemy;
+   public GameObject spawnPOS;
 
-    // Use this for initialization
-    void Start()
-    {
-    }
+   // Use this for initialization
+   void Start()
+   {
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+   // Update is called once per frame
+   void Update()
+   {
+   }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.name.Equals("Kira"))
-        {
-            GameObject tempEnemy = Instantiate(theEnemy, spawnPOS.transform.position, Quaternion.identity) as GameObject;
-        }
-    }
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.name.Equals("Kira"))
+      {
+         Instantiate(theEnemy, spawnPOS.transform.position, Quaternion.identity);
+      }
+   }
 }
