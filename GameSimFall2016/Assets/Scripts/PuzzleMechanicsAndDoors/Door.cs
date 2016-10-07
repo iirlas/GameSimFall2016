aeed1432629ab9fa9fc5e6a5b90e3db88f666882
psipes <psipes@student.alamo.edu>
2016-10-07 15:42:12 -0500

    PAS TOOK STATIC OFF OF DOORS. THEY HAVE TO MOVE. STATIC MEANS NO MOVEMENT. DON'T CLICK BUTTONS YOU DON'T KNOW WHAT THEY MEAN. Also, doors now animate. you're welcome.

--- GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/Door.cs
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/Door.cs
@@ -12,19 +12,42 @@ public class Door : MonoBehaviour
    public int numOfPuzzles;
    public static int puzzlesSolved;
 
+    private bool openDoor;
+    private float speed;
+    private Vector3 destination; 
+
    // Use this for initialization
    void Awake()
    {
       puzzlesSolved = 0;
-
+        openDoor = false;
+        speed = 2f;
+        destination = new Vector3(this.transform.position.x, 
+                                  (this.gameObject.GetComponent<Collider>().bounds.size.y + this.transform.position.y), 
+                                    this.transform.position.z);
    }
+
+    void Update()
+    {
+        if (openDoor)
+        {
+            if (transform.position.y < destination.y)
+            {
+                transform.position = Vector3.Lerp(transform.position,
+                                          destination,
+                                          speed * 3.0f * Time.deltaTime);
+
+            }
+        }
+    }
 
    // Update is called once per frame
    public void OnEvent(BasicTrigger trigger)
    {
       //if (trigger.message == "doorOpen")
       {
-         gameObject.SetActive(false);
+            openDoor = true;
+
       }
 
    }
