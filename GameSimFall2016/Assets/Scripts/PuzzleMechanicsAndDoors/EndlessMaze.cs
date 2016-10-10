f6342db4f8caa22598046869888f96ac15ac4f44
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-10-10 18:08:42 -0500

    NRC:  Updated EndlessMaze prefab, and added an EndlessMaze.cs script.

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/EndlessMaze.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessMaze : MonoBehaviour
+{
+   public GameObject warpPos;
+
+   //==========================================================================
+   // Use this for initialization
+   void Awake()
+   {
+   }
+
+   //==========================================================================
+   // Use this for initialization through other classes
+   void Start()
+   {
+   }
+
+   //==========================================================================
+   // Update is called once per frame
+   void Update()
+   {
+   }
+
+   //==========================================================================
+   // Update is called once per frame
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.name.Equals("Kira"))
+      {
+         foreach (Player unit in PlayerManager.getInstance().players)
+         {
+            unit.gameObject.transform.position = warpPos.transform.position;
+         }
+      }
+   }
+}
