e2aafe56795ea062dd4b1ebf4be2e6ab846aec0d
psipes <psipes@student.alamo.edu>
2016-10-19 14:14:14 -0500

    PAS today is not my day.

---
+++ GameSimFall2016/Assets/Scripts/Utilities/TurnOnOff.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOnOff : MonoBehaviour
+{
+    public GameObject darkness;
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+
+            darkness.SetActive(true);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+            darkness.SetActive(false);
+    }
+
+
+}
