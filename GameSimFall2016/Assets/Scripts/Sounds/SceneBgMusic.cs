726ab733276eacfca09ffd143a94236ea62be5b1
psipes <psipes@student.alamo.edu>
2016-10-21 20:44:27 -0500

    PAS maybe new sound thing. Maybe?

---
+++ GameSimFall2016/Assets/Scripts/Sounds/SceneBgMusic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneBgMusic : MonoBehaviour {
+
+    public AudioSource levelMusic;
+    public AudioClip secondaryMusic;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1 && this.levelMusic.isPlaying == false)
+        {
+            Destroy(this.gameObject);
+            this.gameObject.GetComponentInParent<SoundScenePlay>().enabled = false;
+        }
+
+        levelMusic.Stop();
+        levelMusic.clip = secondaryMusic;
+        levelMusic.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
