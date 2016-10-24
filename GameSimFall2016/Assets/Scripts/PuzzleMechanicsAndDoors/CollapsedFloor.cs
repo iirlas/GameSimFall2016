85543996b432c17a925075b4542bfcd83c79bae3
psipes <psipes@student.alamo.edu>
2016-10-24 14:00:20 -0500

    PAS drop floor

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/CollapsedFloor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollapsedFloor : MonoBehaviour {
+
+    public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Kira"))
+        {
+            floor.SetActive(false);
+        }
+    }
+}
