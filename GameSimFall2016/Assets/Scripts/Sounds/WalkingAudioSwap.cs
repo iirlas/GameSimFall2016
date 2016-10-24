2763577f19237b4cfbb350f8620cc8101fe94cde
jmolina112 <jacintomolina@rocketmail.com>
2016-10-24 13:18:22 -0500

    Uploading Sound Scripts

---
+++ GameSimFall2016/Assets/Scripts/Sounds/WalkingAudioSwap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class WalkingAudioSwap : MonoBehaviour {
+
+
+	public AudioSource walkingSound;
+	public AudioClip walkingOnGrass;
+	public AudioClip walkingOnTile;
+
+	string SceneName = "";
+
+	void Awake () {
+		SceneName = EditorSceneManager.GetActiveScene ().name;
+		if (SceneName.Equals ("TestStartingArea")) {
+			walkingSound.clip = walkingOnGrass;
+		} else {
+			walkingSound.clip = walkingOnTile;
+		}
+
+	}
+}
---
+++ GameSimFall2016/Assets/Scripts/Sounds/waterWalkSwitch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterWalkSwitch : MonoBehaviour {
+
+	public Material waterMaterial;
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider col){
+
+		Debug.Log ("Enters Collider");// is working jsut need to determine the name of the next level of the object then to get the different materials.
+
+		if(col.gameObject.tag.Equals("LevelFloor")){
+			Debug.Log ("Made it to the Level Floor");
+			if (col.GetComponent<MeshRenderer>().material == waterMaterial) {
+				Debug.Log ("Stepping on the water");
+			}
+		}
+
+	}
+}
