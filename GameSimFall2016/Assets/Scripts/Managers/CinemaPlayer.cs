afb9525cc4a05479d82f7f58c1bd041547bbbdd1
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-10-27 21:52:25 -0500

    NRC:  Created CinemaPlayer.cs and SoundManager.cs, currently needs to be implemented.  Director.prefab has been updated to include SoundManager.cs.  CinemaPlayer.cs will be scene dependent and be on it's own object.  SoundManager.cs component within Director.prefab has been populated with a handful of currently used Audio Clips.  CinemaPlayer.cs takes videos in the form of a MovieTexture, which is made by taking a video clip and turning it into a texture.

---
+++ GameSimFall2016/Assets/Scripts/Managers/CinemaPlayer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class CinemaPlayer : Singleton<CinemaPlayer>
+{
+
+   public MovieTexture video;
+   public bool playOnAwake;
+
+   private UnityEvent onPlay;
+   private UnityEvent onPause;
+   private UnityEvent onStop;
+
+   // Use this for initialization
+   override protected void Init()
+   {
+
+   }
+   
+   // Use this for post-initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   // Plays the video
+   public void play()
+   {
+   }
+
+   // Pause the video
+   public void pause()
+   {
+   }
+
+   // Stop the video
+   public void stop()
+   {
+   }
+}
---
+++ GameSimFall2016/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : Singleton<SoundManager>
+{
+   public AudioSource audioSource;
+   public List<AudioClip> audioClips;
+
+   // Use this for initialization
+   override protected void Init()
+   {
+
+   }
+
+   // Use this for post-initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   // 
+   public void playEffect(string name)
+   {
+
+   }
+
+   public void playMusic(string name)
+   {
+
+   }
+
+   public void stop(string name)
+   {
+
+   }
+
+   public void stopAll()
+   {
+
+   }
+}
