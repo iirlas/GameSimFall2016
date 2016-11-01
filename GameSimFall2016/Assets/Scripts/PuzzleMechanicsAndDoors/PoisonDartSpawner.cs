b157179a8e4e841a5b7c83337694b94f74288bcd
iirlas <iirlas@student.alamo.edu>
2016-10-31 21:51:36 -0500

    Added PoisonDartSpawner. Revised PoisonDartWall.prefab. Revised PoisonDart.prefab.

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/PoisonDartSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoisonDartSpawner : MonoBehaviour {
+
+    private List<Transform> spawners = new List<Transform>();
+
+    public GameObject poisonDartPrefab;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        for ( int index =0; index < transform.childCount; index++ )
+        {
+            spawners.Add(transform.GetChild(index));
+        }
+	}
+
+    // Update is called once per frame
+    public void fire ()
+    {
+        foreach ( Transform spawner in spawners )
+        {
+            GameObject poisonDart = Instantiate(poisonDartPrefab, spawner.position, spawner.rotation) as GameObject;
+            poisonDart.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        }
+    }
+}
