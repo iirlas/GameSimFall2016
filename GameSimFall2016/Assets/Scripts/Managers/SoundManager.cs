34f21aedf8cf29614c4d6a28b97249138d142c01
iirlas <iirlas@student.alamo.edu>
2016-10-31 19:54:23 -0500

    Revised SoundManager, Implemented.

--- GameSimFall2016/Assets/Scripts/Managers/SoundManager.cs
+++ GameSimFall2016/Assets/Scripts/Managers/SoundManager.cs
@@ -6,43 +6,34 @@ public class SoundManager : Singleton<SoundManager>
 {
    public AudioSource audioSource;
    public List<AudioClip> audioClips;
-
+   
+   
    // Use this for initialization
    override protected void Init()
    {
-
-   }
-
-   // Use this for post-initialization
-   void Start()
-   {
-
-   }
-
-   // Update is called once per frame
-   void Update()
-   {
-
    }
 
    // 
    public void playEffect(string name)
    {
-
+       
+       AudioClip localAudioClip = audioClips.Find(audioClip => { return (audioClip.name.Equals(name)); });
+       audioSource.PlayOneShot(localAudioClip);
    }
 
    public void playMusic(string name)
    {
-
+       audioSource.clip = audioClips.Find(audioClip => { return (audioClip.name.Equals(name)); });
+       audioSource.Play();
    }
 
    public void stop(string name)
    {
-
+       audioSource.Stop();
    }
 
    public void stopAll()
    {
-
+       audioSource.Stop();
    }
 }
