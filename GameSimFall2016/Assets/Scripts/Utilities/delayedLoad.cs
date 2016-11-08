843a69bfdb059dd3dec9b2186a7db111a9236ce8
Nathan Contreras <nContreras40@users.noreply.github.com>
2016-11-07 18:59:50 -0600

    NRC:  Made intro cinematic play sounds and work/load the scene

---
+++ GameSimFall2016/Assets/Scripts/Utilities/delayedLoad.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class delayedLoad : MonoBehaviour
+{
+   [Tooltip("How long to wait in seconds before loading the next scene.")]
+   public float timeToWait = 0.0f;
+   [Tooltip("The desired scene to load")]
+   public string sceneToLoad = "err";
+
+   private float dTime = 0.0f;
+   private bool start = false;
+
+   //==========================================================================
+   // Use this for initialization
+   void Start()
+   {
+   }
+
+   //==========================================================================
+   // Update is called once per frame
+   void Update()
+   {
+      if (start)
+      {
+         dTime += Time.deltaTime;
+         if (dTime >= timeToWait)
+         {
+            loadScene();
+         }
+      }
+   }
+
+   //==========================================================================
+   // Starts the timer
+   public void beginTimer()
+   {
+      this.start = true;
+   }
+
+   //==========================================================================
+   // Loads the scene
+   public void loadScene()
+   {
+      SceneManager.LoadScene(sceneToLoad); 
+   }
+}
