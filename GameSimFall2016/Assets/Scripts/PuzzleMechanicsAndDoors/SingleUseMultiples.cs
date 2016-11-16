0541182779cd95a565b8144bce256475c426d159
psipes <psipes@student.alamo.edu>
2016-11-16 11:09:48 -0600

    PAS possible working script...

---
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/SingleUseMultiples.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleUseMultiples : MonoBehaviour {
+   //public int size;
+   public GameObject[] startPos;
+   public GameObject[] endPos;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.name.Equals("Kira"))
+      {
+         for (int i = 0; i < startPos.Length; i++)
+         {
+            if (startPos[i].transform.position != endPos[i].transform.position)
+            {
+               startPos[i].transform.position = endPos[i].transform.position;
+            }
+         }
+      }
+   }
+}
