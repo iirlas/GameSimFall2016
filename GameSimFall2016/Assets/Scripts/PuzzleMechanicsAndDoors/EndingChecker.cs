6e7613dbb8b0b8355604f6f3bfb188e0c544c4ad
ToonTalker <ToonTalker@gmail.com>
2016-11-18 21:42:51 -0600

    Dylan added new rooms to the final puzzles and got the token check door to work.

--- GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/EndingChecker.cs
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/EndingChecker.cs
@@ -3,20 +3,18 @@ using System.Collections;
 
 public class EndingChecker : MonoBehaviour {
 
-   public GameObject doorGood;
-   public GameObject doorMeh;
+   public Door doorGood;
+   public Door doorMeh;
    private GameObject chosenDoor;
    private Vector3 destination;
    private bool opendoor;
    private float speed;
-
-   //TAKE THIS OUT LATER
-   public int numOfItems;
+   
 
 	// Use this for initialization
 	void Awake () {
-      opendoor = false;
-      speed = 2f;
+      //opendoor = false;
+      //speed = 2f;
 
 
 	
@@ -24,17 +22,17 @@ public class EndingChecker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-      if (opendoor)
-      {
-         if (chosenDoor.transform.position.y < destination.y)
-         {
-            chosenDoor.transform.position = Vector3.Lerp(chosenDoor.transform.position,
-                                                         destination,
-                                                         speed * 3.0f * Time.deltaTime);
+      //if (opendoor)
+      //{
+      //   if (chosenDoor.transform.position.y < destination.y)
+      //   {
+      //      chosenDoor.transform.position = Vector3.Lerp(chosenDoor.transform.position,
+      //                                                   destination,
+      //                                                   speed * 3.0f * Time.deltaTime);
 
-         }
-         opendoor = false;
-      }
+      //   }
+      //   opendoor = false;
+      //}
 	
 	}
 
@@ -43,22 +41,18 @@ public class EndingChecker : MonoBehaviour {
    {
       if (other.tag.Equals("Player"))
       {
-         //ADD ME BACK IN LATER: if (Inventory.getInstance()["BunnyToken"] == 9)
-         if(numOfItems == 9)
+         if (Inventory.getInstance()["BunnyToken"] >= 9)
          {
-            chosenDoor = doorGood;
-            destination = new Vector3(doorGood.transform.position.x, 
-                                      (doorGood.gameObject.GetComponent<Collider>().bounds.size.y + doorGood.transform.position.y),
-                                      doorGood.transform.position.z);
-            opendoor = true;
+            doorGood.OnEvent(null);
          }
          else
          {
-            chosenDoor = doorMeh;
-            destination = new Vector3(doorMeh.transform.position.x,
-                                      (doorMeh.gameObject.GetComponent<Collider>().bounds.size.y + doorMeh.transform.position.y),
-                                      doorMeh.transform.position.z);
-            opendoor = true;
+            doorMeh.OnEvent(null);
+            //chosenDoor = doorMeh;
+            //destination = new Vector3(doorMeh.transform.position.x,
+            //                          (doorMeh.gameObject.GetComponent<Collider>().bounds.size.y + doorMeh.transform.position.y),
+            //                          doorMeh.transform.position.z);
+            //opendoor = true;
          }
 
       }
