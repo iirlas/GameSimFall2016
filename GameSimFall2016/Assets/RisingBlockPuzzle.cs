e5b5fe835740258412ffd456356464e3c2c02f6c
ToonTalker <ToonTalker@gmail.com>
2016-11-23 10:43:54 -0600

    Dylan was dumb and forgot to commit the dice pillar and is doing it now.

---
+++ GameSimFall2016/Assets/RisingBlockPuzzle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RisingBlockPuzzle : MonoBehaviour {
+
+   public BasicTrigger trigger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      if (StatusManager.getInstance().health <= 0)
+      {
+         trigger.OnAction();
+      }
+   }
+
+}
