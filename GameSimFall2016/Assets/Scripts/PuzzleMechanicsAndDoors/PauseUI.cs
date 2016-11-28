b097d2098dea3405929e83f868439f0cd915b6af
ninjawithglasses <legendarysparda09@gmail.com>
2016-11-28 14:44:20 -0600

    JCG: metas for kira's bump map and texture

--- GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/PauseUI.cs
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/PauseUI.cs
@@ -1,76 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-/*
- * Tearbale terable class 
-*/
 
-public class PauseUI : Singleton<PauseUI>
-{
-    private Canvas pauseCanvas;
+public class PauseUI : Singleton<PauseUI> {
 
-    private List<Component> pausedItems = new List<Component>();
 
-    // Use this for initialization
-    override protected void Init()
-    {
-        pauseCanvas = GetComponent<Canvas>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-    public void pauseToggle()
-    {
-        pauseSet(!pauseCanvas.enabled);
-    }
+   private Canvas pauseCanvas;
 
-    public void pauseSet(bool status)
-    {
-        pauseCanvas.enabled = status;
+   private List<Component> pausedItems = new List<Component>();
 
-        if (pauseCanvas.enabled)
-        {
-            Debug.Log("Za Warudo");
-            Time.timeScale = 1;
-            foreach (Component component in GameObject.FindObjectsOfType<Component>())
+	// Use this for initialization
+	override protected void Init () {
+      pauseCanvas = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   public void pauseToggle()
+   {
+      pauseSet(!pauseCanvas.enabled);
+   }
+
+   public void pauseSet(bool status)
+   {
+      pauseCanvas.enabled = status;
+
+      if (pauseCanvas.enabled)
+      {
+         Debug.Log("Za Warudo");
+         Time.timeScale = 0;
+         foreach (Component component in GameObject.FindObjectsOfType<Behaviour>())
+         {
+            if (!(component is Renderer) &&
+                !(component is Camera) &&
+                (component as Behaviour).transform.root != transform)
             {
-                bool isValid = !(component is Camera) && !(component is Renderer) &&
-                               component.transform.root != transform.root;
-                if (isValid)
-                {
-                    pausedItems.Add(component);
+               pausedItems.Add(component);
+               (component as Behaviour).enabled = false;
+            }
+         }
 
-                    //component is a behaviour
-                    if (component is Behaviour && (component as Behaviour).enabled && 
-                        (component as Behaviour).transform.root != transform.root)
-                    {
-                        (component as Behaviour).enabled = false;
-                    }
-                    else if ( component is Rigidbody )
-                    {
-                        (component as Rigidbody).Sleep();
-                    }
-                }
-            }
-        }
-        else
-        {
-            //pauseCanvas.transform.FindChild("ControlsImage").GetComponent<UnityEngine.UI.Image>().enabled = false;		
-            Time.timeScale = 1;
-            foreach (Component component in pausedItems)
-            {
-                if ( component is Behaviour )
-                {
-                    (component as Behaviour).enabled = true;
-                }
-                else if (component is Rigidbody)
-                {
-                    (component as Rigidbody).WakeUp();//wake me up...
-                }
-            }
-            pausedItems.Clear();
-        }
-    }
+      }
+      else
+      {
+         //pauseCanvas.transform.FindChild("ControlsImage").GetComponent<UnityEngine.UI.Image>().enabled = false;
+         Time.timeScale = 1;
+         foreach (Component component in pausedItems)
+         {
+            (component as Behaviour).enabled = true;
+         }
+         pausedItems.Clear();
+      }
+   }
 }
