03108a4cf27b8fb971c8c205537875ad9646d3a8
ninjawithglasses <legendarysparda09@gmail.com>
2016-11-30 12:39:28 -0600

    JCG: The thing that makes this not not go boom

---
+++ GameSimFall2016/Assets/Scripts/Utilities/ExplosiveSingleton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+abstract public class ExplosiveSingleton<Type> : MonoBehaviour
+    where Type : MonoBehaviour
+{
+   static private Type ourInstance;
+
+   //hide the property using new keyword to 
+   //create the singleton when one is not avaliable
+   static protected bool isCreatedWhenMissing
+   {
+      get { return false; }
+   }
+
+   static public Type getInstance()
+   {
+      if (ourInstance == null)
+      {
+         ourInstance = GameObject.FindObjectOfType<Type>();
+         if (ourInstance == null)
+         {
+            throw new System.Exception("Singleton [" + typeof(Type) + "] not in the current scene!");
+         }
+      }
+      return ourInstance;
+   }
+
+   void Awake()
+   {
+      ourInstance = getInstance();
+      if (ourInstance != this)
+      {
+         Destroy(ourInstance.gameObject);
+         ourInstance = getInstance();
+      }
+      else
+      {
+         Init();
+      }
+   }
+
+   protected abstract void Init();
+}
