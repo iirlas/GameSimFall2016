ab13087898c0d879f238b2c48354309ffe140c78
psipes <psipes@student.alamo.edu>
2016-12-05 22:23:09 -0600

    pas please...

---
+++ GameSimFall2016/Assets/Scripts/Utilities/FlipThisManyTimes.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipThisManyTimes : MonoBehaviour {
+
+    public GameObject textStuff;
+    public GameObject flipping;
+    public GameObject button;
+    private Animator textAnim;
+    private Animator pageFlip;
+    
+   
+
+	// Use this for initialization
+	void Awake () {
+        textAnim = textStuff.GetComponent<Animator>();
+        pageFlip = flipping.GetComponent<Animator>();
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (textAnim.GetCurrentAnimatorStateInfo(0).IsName("StaticPage"))
+        {
+            pageFlip.SetBool("finished", true);
+            button.SetActive(true);
+
+        }
+
+
+       
+        
+	
+	}
+    
+}
