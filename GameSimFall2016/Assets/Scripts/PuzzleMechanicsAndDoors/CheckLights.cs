6a8719adef7a5e29d9c75722fcb325a3d9cf2d16
iirlas <iirlas@student.alamo.edu>
2017-04-20 17:51:27 -0500

    Revised CheckLights. Added SuccessChime when completing puzzle.

--- GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/CheckLights.cs
+++ GameSimFall2016/Assets/Scripts/PuzzleMechanicsAndDoors/CheckLights.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Linq;
 using System.Collections;
 
 //========================================================================================================
@@ -8,12 +9,10 @@ using System.Collections;
 
 public class CheckLights : MonoBehaviour {
     [Tooltip("Door to Exit from")]
-    public GameObject exitDoor;
+    //public GameObject exitDoor;
+	public UnityEngine.Events.UnityEvent completeEvent;
     [Tooltip ("Lights to be counted")]
     public Light[] desiredLights;
-    private Vector3 destination;
-    private bool openDoor;
-    private float speed;
 
     private int count;
     
@@ -21,47 +20,18 @@ public class CheckLights : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         count = 0;
-        speed = 2f;
-        destination = new Vector3(exitDoor.transform.position.x,
-                                (exitDoor.gameObject.GetComponent<Collider>().bounds.size.y + exitDoor.transform.position.y * 2),
-                                  exitDoor.transform.position.z);
-        openDoor = false;
     }
 
     // Update is called once per frame
 	void Update () {
-        foreach (Light myLight in desiredLights)
-        {
-            if (myLight.intensity > 0)
-                count++;
-        }
-
-        if (count == desiredLights.Length)
-        {
-            //Debug.Log("countMet");
-            openDoor = true;
-            
-
-        }
-        else
-        {
-            count = 0;
-        }
-
-        if (openDoor)
-        {
-            if (exitDoor.transform.position.y < destination.y)
-            {
-                exitDoor.transform.position = Vector3.Lerp(exitDoor.transform.position,
-                                          destination,
-                                          speed * 3.0f * Time.deltaTime);
-
-            }
-        }
-
-        //if (exitDoor.transform.position.y >= destination.y)
-        //{
-        //    Destroy(exitDoor);
-        //}	
+		if (count != -1) {
+			count = desiredLights.Count ((Light light) => {
+				return (light.intensity > 0);
+			});
+			if (count == desiredLights.Length) {
+				completeEvent.Invoke ();
+				count = -1;
+			}
+		}
 	}
 }
